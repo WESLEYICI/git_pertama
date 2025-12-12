1fdf5a4aa914c56d7d3d1bf3314ccd8ff9215097
wesley <wesleyici078@gmail.com>
2025-12-12 20:32:35 +0700

    menyelesaikan soal no.6

--- tugas.panen.py
+++ tugas.panen.py
@@ -41,13 +41,19 @@ data_panen = {
     }
 }
 
-# tugas soal no.2
+# -----------------------------
+# SOAL NO.2
+# -----------------------------
 print("Jagung lokasi2:", data_panen['lokasi2']['hasil_panenn']['jagung'])
 
-# tugas soal no.3
+# -----------------------------
+# SOAL NO.3
+# -----------------------------
 print("Nama lokasi3:", data_panen['lokasi3']['nama_lokasi'])
 
-# tugas soal no.4
+# -----------------------------
+# SOAL NO.4
+# -----------------------------
 padi_lokasi1 = data_panen['lokasi1']['hasil_panenn']['padi']
 kedelai_lokasi1 = data_panen['lokasi1']['hasil_panenn']['kedelai']
 
@@ -63,23 +69,32 @@ kedelai_lokasi4 = data_panen['lokasi4']['hasil_panenn']['kedelai']
 padi_lokasi5 = data_panen['lokasi5']['hasil_panenn']['padi']
 kedelai_lokasi5 = data_panen['lokasi5']['hasil_panenn']['kedelai']
 
-print("Variabel padi dan kedelai setiap lokasi berhasil dibuat.")
+print("Soal No.4 selesai: variabel padi & kedelai tiap lokasi dibuat.")
 
-#tugas soal no.5
-padi_lokasi1 = data_panen['lokasi1']['hasil_panenn']['padi']
-kedelai_lokasi1 = data_panen['lokasi1']['hasil_panenn']['kedelai']
+# -----------------------------
+# SOAL NO.5
+# -----------------------------
+padi_semua_lokasi = [
+    padi_lokasi1, padi_lokasi2, padi_lokasi3, padi_lokasi4, padi_lokasi5
+]
 
-padi_lokasi2 = data_panen['lokasi2']['hasil_panenn']['padi']
-kedelai_lokasi2 = data_panen['lokasi2']['hasil_panenn']['kedelai']
+kedelai_semua_lokasi = [
+    kedelai_lokasi1, kedelai_lokasi2, kedelai_lokasi3, kedelai_lokasi4, kedelai_lokasi5
+]
 
-padi_lokasi3 = data_panen['lokasi3']['hasil_panenn']['padi']
-kedelai_lokasi3 = data_panen['lokasi3']['hasil_panenn']['kedelai']
+print("Soal No.5 selesai: variabel daftar padi & kedelai semua lokasi dibuat.")
 
-padi_lokasi4 = data_panen['lokasi4']['hasil_panenn']['padi']
-kedelai_lokasi4 = data_panen['lokasi4']['hasil_panenn']['kedelai']
+# -----------------------------
+# SOAL NO.6
+# -----------------------------
+print("\nSoal No.6: Mengecek lokasi yang memerlukan perhatian khusus:\n")
 
-padi_lokasi5 = data_panen['lokasi5']['hasil_panenn']['padi']
-kedelai_lokasi5 = data_panen['lokasi5']['hasil_panenn']['kedelai']
+for lokasi, info in data_panen.items():
+    padi = info['hasil_panenn']['padi']
+    jagung = info['hasil_panenn']['jagung']
+    nama = info['nama_lokasi']
 
-print("Variabel padi dan kedelai setiap lokasi berhasil dibuat.")
-
+    if padi > 1300 or jagung > 800:
+        print(f"{nama} ({lokasi}) memerlukan perhatian khusus!")
+    else:
+        print(f"{nama} ({lokasi}) dalam kondisi normal.")
